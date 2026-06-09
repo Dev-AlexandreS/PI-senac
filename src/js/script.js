151f3f903a23b2b88c32419a921518e28028e31c
Dev-AlexandreS <alegamerth@gmail.com>
2026-06-08 21:01:59 -0300

    ajustes de footer e wa.me

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,27 @@
 const cta = document.getElementById("cta");
+const telefone = "5511913020257";
 
 cta.addEventListener("click", function () {
     console.log("Botão clicado!");
     document.getElementById("offers").scrollIntoView({
         behavior: "smooth"
     });
+});
+
+
+document.querySelectorAll(".product").forEach((produto) => {
+  produto.addEventListener("click", () => {
+    const nomeProduto = produto
+      .querySelector(".product-title")
+      .textContent
+      .trim();
+
+    const mensagem = `Tenho interesse no produto ${nomeProduto}, está disponível?`;
+
+    const url = `https://wa.me/${telefone}?text=${encodeURIComponent(
+      mensagem
+    )}`;
+
+    window.open(url, "_blank");
+  });
 });
